f81037130d818d3e8b1d9bb6dc7d85cde19684c7
kenvap <ckonst@gmail.com>
2025-01-18 22:12:09 -0600

    Initial Commit: A

---
+++ main.py
@@ -0,0 +1,36 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame #noqa: F401
+from constants import * # noqa: F403
+#from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}") # noqa: F405
+    print(f"Screen height: {SCREEN_HEIGHT}") # noqa: F405
+    # Initialize pygame
+    pygame.init()
+
+    # Set up the display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #noqa: F405
+    pygame.display.set_caption("Asteroids Game")
+
+    # main loop
+    running = True
+    while running:  # noqa: F405
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill the screen with a color (RGB)
+        screen.fill((0, 0, 0))
+
+        # Update the display
+        pygame.display.flip()
+    
+    #Quit pygame
+    pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
